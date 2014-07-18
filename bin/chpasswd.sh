c0a6e9ee2a61b0a5eb3dca5c77f91e526cf55308
Neo Chen <openunix@163.com>
2014-07-18 17:43:57 +0800

    modified:   chpasswd.sh

--- bin/chpasswd.sh
+++ bin/chpasswd.sh
@@ -2,8 +2,11 @@
 datetime=`date +%Y-%m-%d" "%H":"%M`
 email="neo.chan@live.com"
 #password=$(cat /dev/urandom | tr -cd [:alnum:] | fold -w30 | head -n 1)
-password=$(date -u "+%m$1%d$2%H$3%M" | base64 | tr -d "=" | fold -w30 | head -n 1)
+string=$(date -u "+%Y$1%m$2%d$3%H$4%M")
+password=$(echo $string | md5sum | cut -c 2-9 | base64 | tr -d "=" | cut -c 1-32)
+echo $string
 echo $password > ~/.lastpasswd
+exit
 echo $password | passwd www --stdin > /dev/null
 
 #for pts in $(w | awk -F' ' '{if ($1 == "www") print $2}')
