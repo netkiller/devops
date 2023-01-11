0dfc117a428f4add2f3ac4f0d02edf7cdb3f8d5b
Neo Chan <netkiller@msn.com>
2023-01-11 16:03:43 +0800

    Changes to be committed: 	modified:   .gitignore 	modified:   container/docker/elk/logstash.py

--- container/docker/elk/logstash.py
+++ container/docker/elk/logstash.py
@@ -27,31 +27,34 @@ dockerfile.image('docker.elastic.co/logstash/logstash:8.6.0').run(
 ).copy('pipeline/', '/usr/share/logstash/pipeline/').copy('config/', '/usr/share/logstash/config/').workdir('/usr/share/logstash')
 
 logstash = Services(project)
-# openresty.image('openresty/openresty:alpine')
-# openresty.build(dockerfile)
+# logstash.image('logstash:latest')
+# logstash.build(dockerfile)
 logstash.image('docker.elastic.co/logstash/logstash:8.6.0')
 logstash.container_name(project)
 logstash.restart('always')
 # logstash.hostname('www.netkiller.cn')
 # openrelogstashsty.extra_hosts(extra_hosts)
-# logstash.extra_hosts(['db.netkiller.cn:127.0.0.1','cache.netkiller.cn:127.0.0.1','api.netkiller.cn:127.0.0.1'])
-logstash.environment(['TZ=Asia/Shanghai'])
-logstash.ports(['80:80', '443:443'])
-# openresty.depends_on('test')
+logstash.extra_hosts(['elasticsearch:127.0.0.1'])
+logstash.environment(['TZ=Asia/Shanghai','XPACK_MONITORING_ENABLED=false','LOG_LEVEL=info'])
+logstash.ports(['12201:12201/udp', '12201:12201/tcp'])
+#logstash.ports(['12201:12201','4567:4567'])
+# logstash.depends_on('test')
 logstash.working_dir('/usr/share/logstash')
+logstash.user('root')
 logstash.volumes(
     [
         '/srv/logstash/pipeline/:/usr/share/logstash/pipeline/',
+        #'/srv/logstash/config/logstash.yml:/usr/share/logstash/config/logstash.yml:rw',
         '/srv/logstash/logs/:/usr/share/logstash/logs/',
         '/opt/log/:/opt/log/',
+        '/proc:/proc','/sys:/sys'
     ]
-)
+).privileged()
 
 development = Composes('development')
 development.workdir('/var/tmp/development')
 development.version('3.9')
 development.services(logstash)
-
 
 if __name__ == '__main__':
     try:
