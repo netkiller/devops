08f3e45932b5ef3372c6cc80998ba5a6decf4fb3
Neo Chan <netkiller@msn.com>
2023-03-08 21:13:41 +0800

    调整bar

--- demo/gantt.py
+++ demo/gantt.py
@@ -23,12 +23,14 @@ class Gantt:
     itemHeight = 30
     itemWidth = 30
     barHeight = 20
-    progressHeight = 15
+    progressHeight = 14
 
     data = [
-        {'title': '汉字', 'begin': '2023-03-01', 'end': '2023-03-05'},
-        {'title': 'Java', 'begin': '2023-03-01', 'end': '2023-03-05'},
-        {'title': 'PHP', 'begin': '2023-03-03', 'end': '2023-03-10'},
+        {'title': '汉字', 'begin': '2023-03-01', 'end': '2023-03-05', 'progress': 3},
+        {'title': 'Java', 'begin': '2023-03-01',
+            'end': '2023-03-05', 'progress': 2},
+        {'title': 'PHP', 'begin': '2023-03-03',
+            'end': '2023-03-10', 'progress': 5},
         {'title': 'Go', 'begin': '2023-03-10', 'end': '2023-03-20'},
         {'title': 'Python', 'begin': '2023-03-06', 'end': '2023-03-08'},
         {'title': 'Swift', 'begin': '2023-03-06', 'end': '2023-03-20', 'subitem': [
@@ -40,19 +42,19 @@ class Gantt:
 
     def __init__(self) -> None:
         self.draw = draw.Drawing(self.canvasWidth, self.canvasHeight)
-        self.draw.append(draw.Rectangle(1, 1, self.canvasWidth,
-                         self.canvasHeight, fill='#eeeeee'))
+        self.draw.append(draw.Rectangle(1, 1, self.canvasWidth - 1,
+                         self.canvasHeight-1, fill='#eeeeee', stroke='black'))
 
-        left = draw.Line(0, 0, 1, self.canvasHeight, stroke='black')
-        top = draw.Line(0, 0, self.canvasWidth, 0, stroke='black')
-        right = draw.Line(self.canvasWidth, 0,
-                          self.canvasWidth, self.canvasHeight, stroke='black')
-        buttom = draw.Line(0, self.canvasHeight, self.canvasWidth,
-                           self.canvasHeight,  stroke='black')
-        self.draw.append(left)
-        self.draw.append(top)
-        self.draw.append(right)
-        self.draw.append(buttom)
+        # left = draw.Line(0, 0, 1, self.canvasHeight, stroke='black')
+        # top = draw.Line(0, 0, self.canvasWidth, 0, stroke='black')
+        # right = draw.Line(self.canvasWidth, 0,
+        #                   self.canvasWidth, self.canvasHeight, stroke='black')
+        # buttom = draw.Line(0, self.canvasHeight, self.canvasWidth,
+        #                    self.canvasHeight,  stroke='black')
+        # self.draw.append(left)
+        # self.draw.append(top)
+        # self.draw.append(right)
+        # self.draw.append(buttom)
 
     def title(self, text):
         group = draw.Group(id='title', fill='none', stroke='none')
@@ -62,9 +64,11 @@ class Gantt:
         self.draw.append(group)
 
     def background(self):
+
         left = self.starting
         top = 80
         offsetX = 0
+        group = draw.Group(id='background')
         for day in range(1, 31):
             # print(day)
             weekday = calendar.weekday(2023, 3, day)
@@ -77,26 +81,27 @@ class Gantt:
             x = left + self.unitWidth * (day-1) + offsetX
 
             if weekday == 6:
-                self.draw.append(draw.Line(x + 30, top - 40,
-                                 x + 30, self.canvasHeight, stroke='black'))
+                group.append(draw.Line(x + 30, top - 40,
+                                       x + 30, self.canvasHeight, stroke='black'))
 
             r = draw.Rectangle(x, top, self.unitWidth,
                                self.canvasHeight, fill=color)
             r.append_title(str(day))
-            self.draw.append(r)
-            self.draw.append(
+            group.append(r)
+            group.append(
                 draw.Text(str(day), 24, x, top + 24, fill='#555555'))
 
             if day:
                 offsetX += self.splitLine
-        line = draw.Line(1, top + 26, self.canvasWidth,
-                         top + 26, stroke='grey')
-        self.draw.append(line)
+        group.append(draw.Line(1, top + 26, self.canvasWidth,
+                               top + 26, stroke='grey'))
+
+        self.draw.append(group)
 
     def item(self, line, subitem=False):
         left = self.starting
         top = 110 + self.itemLine * self.itemHeight + self.splitLine * self.itemLine
-        print(top)
+        # print(top)
 
         begin = datetime.strptime(line['begin'], '%Y-%m-%d').day
         # end = datetime.strptime(line['end'], '%Y-%m-%d').day
@@ -134,19 +139,21 @@ class Gantt:
                 fill='black', stroke='black'))
         else:
 
-            day = 2
             # mask = draw.Mask()
-            # 进度
-            progress = draw.Rectangle(
-                left, top + 5, 30 * day, self.progressHeight, fill='#ccffff')
-            progress.append_title("50%")
-            # mask.append(progress)
 
             # 工时
-            r = draw.Rectangle(left, top +4, right,self.barHeight, fill='#ccccff')
+            r = draw.Rectangle(left, top + 4, right,
+                               self.barHeight, fill='#ccccff')
             r.append_title(line['title'])
             group.append(r)
-            group.append(progress)
+
+            # 进度
+            if 'progress' in line:
+                progress = draw.Rectangle(
+                    left, top + 7, 30 * line['progress'], self.progressHeight, fill='#ccffff')
+                progress.append_title(str(line['progress']))
+                # mask.append(progress)
+                group.append(progress)
 
         # 分割线
         group.append(draw.Lines(self.starting, top + self.itemHeight,
@@ -162,7 +169,7 @@ class Gantt:
                              fill='red', stroke='black', close='true')
         star = draw.Lines(48, 16, 16, 96, 96, 48, 0, 48, 88, 96,
                           stroke='black', fill='red', close='true')
-        print(draw.Text("Test", 20, 5, top + 15, text_anchor='start').get_svg_defs)
+        print(draw.Text("Test", 20, 5, top + 15, text_anchor='start'))
         # self.draw.append(lines)
         # top = 40
         # line = draw.Line(1, top, self.canvasWidth, top, stroke='black')
@@ -186,14 +193,14 @@ class Gantt:
                     self.item(item)
             else:
                 self.item(line)
-        #      = 30 * y
-        #     for x in range(1, 30):
-        #         # print(day)
-        #         offsetX = 31 * x
-        #         r = draw.Rectangle(offsetX, offsetY+150,
-        #                            30, 30, fill='#bbbbbb')
-        #         r.append_title("Our first rectangle")  # Add a tooltip
-        #         self.draw.append(r)
+
+    def save(self):
+        # d.set_pixel_scale(2)  # Set number of pixels per geometry unit
+        # d.set_render_size(400, 200)  # Alternative to set_pixel_scale
+
+        self.draw.save_svg('example.svg')
+        # self.draw.save_png('example.png')
+        # self.draw.rasterize()
 
     def main(self):
 
@@ -201,40 +208,8 @@ class Gantt:
         self.background()
         # self.days()
         self.task()
-        self.test()
-        self.draw.save_svg('example.svg')
+        self.save()
 
 
 gantt = Gantt()
-
 gantt.main()
-
-
-# # Draw an arbitrary path (a triangle in this case)
-# p = draw.Path(stroke_width=2, stroke='lime', fill='black', fill_opacity=0.2)
-# p.M(-10, -20)  # Start path at point (-10, -20)
-# p.C(30, 10, 30, -50, 70, -20)  # Draw a curve to (70, -20)
-# d.append(p)
-
-# # Draw text
-# d.append(draw.Text('Basic text', 8, -10, -35, fill='blue'))  # 8pt text at (-10, -35)
-# d.append(draw.Text('Path text', 8, path=p, text_anchor='start', line_height=1))
-# d.append(draw.Text(['Multi-line', 'text'], 8, path=p, text_anchor='end', center=True))
-
-# # Draw arrows
-# arrow = draw.Marker(-0.1, -0.51, 0.9, 0.5, scale=4, orient='auto')
-# arrow.append(draw.Lines(-0.1, 0.5, -0.1, -0.5, 0.9, 0, fill='red', close=True))
-# p = draw.Path(stroke='red', stroke_width=2, fill='none',
-#         marker_end=arrow)  # Add an arrow to the end of a path
-# p.M(20, 40).L(20, 27).L(0, 20)  # Chain multiple path commands
-# d.append(p)
-
-
-# d.set_pixel_scale(2)  # Set number of pixels per geometry unit
-# d.set_render_size(400, 200)  # Alternative to set_pixel_scale
-
-# d.save_png('example.png')
-
-# Display in Jupyter notebook
-# d.rasterize()  # Display as PNG
-# d  # Display as SVG
