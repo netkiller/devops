dd09114a907868dcc3fff448cdac7187528e4c5e
Neo Chan <netkiller@msn.com>
2023-03-24 08:59:34 +0800

    modified:   netkiller/pipeline.py

--- netkiller/pipeline.py
+++ netkiller/pipeline.py
@@ -18,9 +18,11 @@ from logging import basicConfig
 sys.path.insert(0, '/Users/neo/workspace/devops')
 sys.path.insert(0, '../devops')
 
+
 class Stage:
     def __init__(self) -> None:
         pass
+
 
 class Pipeline:
     Maven = 'maven'
@@ -29,16 +31,17 @@ class Pipeline:
     Yarn = 'yarn'
     Gradle = 'gradle'
 
-    def __init__(self, workspace, logfile = 'debug.log'):
+    def __init__(self, workspace, logfile='debug.log'):
 
         self.container = 'docker'
-        self.registry=None
+        self.registry = None
         self.workspace = workspace
         self.pipelines = {}
-        if not os.path.exists(self.workspace) :
+        if not os.path.exists(self.workspace):
             os.mkdir(self.workspace)
         self.logging = logging.getLogger()
-        logging.basicConfig(level=logging.NOTSET, format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S', filename=logfile, filemode='a')
+        logging.basicConfig(level=logging.NOTSET, format='%(asctime)s %(levelname)-8s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S', filename=logfile, filemode='a')
 
     def begin(self, project):
         self.logging.info("%s %s %s" % ("="*20, project, "=" * 20))
@@ -52,7 +55,7 @@ class Pipeline:
     def env(self, key, value):
         os.putenv(key, value)
         # os.environ['JAVA_HOME'] = '/Library/Java/JavaVirtualMachines/jdk1.8.0_341.jdk/Contents/Home'
-        self.logging.info("%s = %s" % (key,value))
+        self.logging.info("%s = %s" % (key, value))
         return self
 
     def init(self, script):
@@ -61,19 +64,20 @@ class Pipeline:
         return self
 
     def checkout(self, url, branch):
-        self.logging.info("%s = %s" % (url,branch))
+        self.logging.info("%s = %s" % (url, branch))
         if os.path.exists(self.project):
-            git = Git(os.path.join(self.workspace,self.project), self.logging)
+            git = Git(os.path.join(self.workspace, self.project), self.logging)
             git.fetch().checkout(branch).pull().execute()
         else:
             git = Git(self.workspace, self.logging)
             git.option('--branch ' + branch)
             git.clone(url, self.project).execute()
             os.chdir(self.project)
-        self.pipelines['checkout'] = ['pwd','ls']
+        self.pipelines['checkout'] = ['pwd', 'ls']
         return self
+
     def build(self, script):
-        # 
+        #
         # if compiler == self.Maven :
         #     self.pipelines['build'] = ['maven clean package']
         # elif compiler == self.Npm :
@@ -92,24 +96,29 @@ class Pipeline:
         self.pipelines['test'] = script
         self.logging.info("test: %s" % script)
         return self
+
     def docker(self, registry, username=None, password=None):
         self.pipelines['container'] = []
-        self.registry =registry 
+        self.registry = registry
         self.container = 'docker'
-        if username :
-            self.pipelines['container'].append(self.container + " login -u {username} -p{password} {registry}".format(username=username ,password= password,registry = self.registry))
+        if username:
+            self.pipelines['container'].append(self.container + " login -u {username} -p{password} {registry}".format(
+                username=username, password=password, registry=self.registry))
         return self
+
     def podman(self, registry, username=None, password=None):
         self.pipelines['container'] = []
-        self.registry =registry 
+        self.registry = registry
         self.container = 'podman'
-        if username :
-            self.pipelines['container'].append(self.container + " login -u {username} -p{password} {registry}".format(username=username ,password= password,registry = self.registry))
+        if username:
+            self.pipelines['container'].append(self.container + " login -u {username} -p{password} {registry}".format(
+                username=username, password=password, registry=self.registry))
         return self
+
     def dockerfile(self, tag=None, dir=None):
         self.pipelines['dockerfile'] = []
         if self.registry:
-            image = os.path.join(self.registry,self.project)
+            image = os.path.join(self.registry, self.project)
         else:
             image = self.project
 
@@ -118,16 +127,27 @@ class Pipeline:
         else:
             tag = image+':' + datetime.now().strftime('%Y%m%d-%H%M')
 
-        if dir :
+        if dir:
             os.chdir(dir)
-            
-        self.pipelines['dockerfile'].append(self.container + ' build -t '+tag+' .')
-        self.pipelines['dockerfile'].append(self.container + ' tag '+tag+' '+image)
+
+        self.pipelines['dockerfile'].append(
+            self.container + ' build -t '+tag+' .')
+        self.pipelines['dockerfile'].append(
+            self.container + ' tag '+tag+' '+image)
         self.pipelines['dockerfile'].append(self.container + ' push '+tag)
         self.pipelines['dockerfile'].append(self.container + ' push '+image)
         self.pipelines['dockerfile'].append(self.container + ' image rm '+tag)
-        self.pipelines['dockerfile'].append(self.container + ' image rm '+image)
+        self.pipelines['dockerfile'].append(
+            self.container + ' image rm '+image)
         self.logging.info("dockerfile: %s" % self.pipelines['dockerfile'])
+        return self
+
+    def nacos(self, server, username, password, namespace, dataid, group, filepath):
+        self.pipelines['nacos'] = []
+        self.pipelines['nacos'].append("nacos -s {server} -u {username} -p {password} -n {namespace} -d {dataid} -g {group} --push -f {filepath}".format(
+            server=server, username=username, password=password, namespace=namespace, dataid=dataid, group=group, filepath=filepath))
+        self.pipelines['nacos'].append("nacos -s {server} -u {username} -p {password} -n {namespace} -d {dataid} --show".format(
+            server=server, username=username, password=password, namespace=namespace, dataid=dataid))
         return self
 
     def deploy(self, script):
@@ -139,32 +159,33 @@ class Pipeline:
         self.pipelines['startup'] = script
         self.logging.info("startup: %s" % script)
         return self
+
     def stop(self, script):
         self.pipelines['stop'] = script
         self.logging.info("stop: %s" % script)
         return self
+
     def end(self, script=None):
         if script:
             self.pipelines['end'] = script
         try:
-            for stage in ['begin','init', 'checkout', 'build', 'container','dockerfile', 'deploy', 'stop', 'startup', 'end']:
+            for stage in ['begin', 'init', 'checkout', 'build', 'container', 'dockerfile', 'nacos', 'deploy', 'stop', 'startup', 'end']:
                 if stage in self.pipelines.keys():
                     for command in self.pipelines[stage]:
                         rev = subprocess.call(command, shell=True)
                         # rev = subprocess.call(command, shell=True,executable='/bin/bash', env=dict(ENV='/User/neo/.zprofile'))
-                        # if rev.returncode == 0 :
-                        #     status = 'done'
-                        # else:
-                        #     status = 'error'
-                        self.logging.info("command: %s, %s, status: %s" % (rev, command,rev))
+                        self.logging.info(
+                            "command: %s, status: %s" % (command, rev))
                         # if rev != 0 :
                         # raise Exception("{} 执行失败".format(command))
         except KeyboardInterrupt as e:
             self.logging.info(e)
         self.logging.info("="*50)
         return self
+
     def image(self):
-        return(self.image)
+        return (self.image)
+
     def debug(self):
         print(self.pipelines)
         return self
