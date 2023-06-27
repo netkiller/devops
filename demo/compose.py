f63d86feba86927e0b4482e0c28c20d059bac236
neo <netkiller@msn.com>
2023-06-27 18:45:52 +0800

    Changes to be committed: 	deleted:    demo/compose.py 	deleted:    demo/docker/nginx.py 	deleted:    demo/search/elasticsearch.yaml 	deleted:    demo/sendmessage.py 	deleted:    demo/wc.py 	deleted:    demo/yaml2dict.py

--- demo/compose.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-##############################################
-# Home	: http://netkiller.github.io
-# Author: Neo <netkiller@msn.com>
-# Upgrade: 2021-09-05
-##############################################
-user_input = input('Do you like pizza (yes/no): ').lower()
-print(user_input)
